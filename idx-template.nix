cd68b9ff2773da1e6413e8cab175e5fefd509d08
It Just Crashed <151413500+itjustcrashed@users.noreply.github.com>
2025-08-17 15:38:17 -0400

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -17,7 +17,6 @@
     --test-framework ${project-tests} \
     --package ${project-package} \
     --project-name ${project-name} \
-    --use-defaults
 
     # Copy the folder containing the `idx-template` files to the final
     # project folder for the new workspace. ${./.} inserts the directory
