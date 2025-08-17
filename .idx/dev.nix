2f948c9c927093d5726c0201b382bfc798c20a44
It Just Crashed <151413500+itjustcrashed@users.noreply.github.com>
2025-08-17 15:54:00 -0400

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,7 +6,7 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.jdk23
-    pkgs.gradle,
+    pkgs.gradle
     pkgs.kotlin
   ];
   # Sets environment variables in the workspace
