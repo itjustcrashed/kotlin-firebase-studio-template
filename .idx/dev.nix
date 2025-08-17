397d1229026b3c3fc2233f5959165d31c1291b18
It Just Crashed <151413500+itjustcrashed@users.noreply.github.com>
2025-08-17 14:43:59 -0400

    Create dev.nix

---
+++ .idx/dev.nix
@@ -0,0 +1,30 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-25.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.jdk23
+    pkgs.gradle,
+    pkgs.kotlin
+  ];
+  # Sets environment variables in the workspace
+  env = { };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "fwcd.kotlin"
+      "richardwillis.vscode-gradle"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        default.openFiles = [ "build.gradle.kts" ];
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = { };
+  };
+}
